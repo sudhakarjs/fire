220dad92d3442f2c668a259b87779127b89b5ce1
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-01-18 16:57:13 +0530

    Create nature3.c

---
+++ nature3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, num = 0;
+    
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+
+    for(b=1; b <= a; ++b)
+    {
+        num += b;  
+    }
+
+    printf("num = %d",num);
+
+    return 0;
+}
