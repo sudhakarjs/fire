a525f596740bbdae5d51901c8d5e7d3a154b4c05
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-01-18 16:19:03 +0530

    Create consonant.c

---
+++ consonant.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    char c;
+    int isLowercaseVowel, isUppercaseVowel;
+
+    printf("Enter an alphabet: ");
+    scanf("%c",&c);
+    if (isLowercaseVowel || isUppercaseVowel)
+        printf("%c is a vowel.", c);
+    else
+        printf("%c is a consonant.", c);
+    return 0;
+}
