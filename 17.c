67bef18cf8cb9cec1eabfc088d3aa81e17fbd6ab
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-02-08 09:04:08 +0000

    Create 17.c

---
+++ 17.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#$include<conio.h>
+int main()
+{
+int c,j,b,n;
+printf("Enter the first number");
+scanf("%d",&n);
+printf("Enter the last number");
+scanf("%d",&b);
+for(c=n;c<b;c++)
+if(i%j==0)
+{
+for(j=2;j<+i;i++)
+{
+c=1;
+break;
+}
+if(c==0)
+{
+printf("the prime number is &d",c);
+}
+getch();
+return 0;
+}
+
