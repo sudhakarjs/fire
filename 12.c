2497fb1945787cfd4bdfa3646f95203550274255
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-02-08 08:57:18 +0000

    Create 12.c

---
+++ 12.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+int k,a,b,c;
+printf("enter the value : ");
+scanf("%d",&a);
+printf("Enter the power value :");
+scanf("%d",&b);
+ans=1;
+for(k=1;k<=b;k++)
+{
+	c=c*b;
+}
+printf("the value is:%d",c);
+return 0;
+}
