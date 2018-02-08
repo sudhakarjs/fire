e211adb32339affdd760b0af61a58f06eb9b383e
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-02-08 09:08:49 +0000

    Create 20.c

---
+++ 20.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a=1,f=1,x;
+printf("enter the value");
+scanf("%d",&x);
+for(a=1;a<=x;a++)
+{
+f=f*i;
+}
+printf("factorial is:%d",f);
+getch();
+return 0;
+}
