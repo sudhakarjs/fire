7a2388565893fd9e48e440a828bfd18144fc0d13
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-02-08 09:01:27 +0000

    Create 15.c

---
+++ 15.c
@@ -0,0 +1,6 @@
+num1=int(input("Enter upper range:"))
+num2=int(input("Enter lower range:"))
+my_list=[]
+for x in range(num1+1,num2);
+odd_list=list(filter(lambda x:(x%2!=0),my_list))
+print(*odd_list)
